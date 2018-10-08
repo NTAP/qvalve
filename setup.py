ebc5c26812160caf44a55f523cf0b67230559182
Maxime Piraux <maxime.piraux@uclouvain.be>
2018-10-08 16:54:41 +0200

    Adds textx to setup.py dependencies

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name='qvalve',
       author_email='lars@eggert.org',
       packages=setuptools.find_packages(),
       scripts=['bin/qvalve'],
+      install_requires=['textx'],
       classifiers=(
               "Programming Language :: Python :: 3",
               "License :: OSI Approved :: BSD License",
